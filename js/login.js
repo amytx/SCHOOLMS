ac9c58fa16f79ea7390d5fa686e2b77b1350dcd5
Amy Sun <tzsunamy@gmail.com>
2016-11-27 09:52:55 -0500

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  $(".hidestuff").hide();
+  $(".signup_form").hide();
+  $(".login_form").slideDown(3000);
+}
+function myFunction2() {
+  $(".hidestuff").hide();
+  $(".login_form").hide();
+  $(".signup_form").slideDown(3000);
+}
+
+function myFunction3() {
+  $(".hidestuff").slideDown(3000);
+  $(".login_form").hide();
+  $(".signup_form").hide();
+}
